0db751529cda28d4963378e1d31569a7d11536c8
Stefan Maierhofer <sm@stefanmaierhofer.com>
2015-08-26 09:25:18 +0200

    add src/Examples/Examples.csproj

---
+++ src/Examples/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Async;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Examples
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Example1().Wait();
+        }
+
+        static async Task Example1()
+        {
+            var work = Enumerable.Range(1, 5)
+                .ToAsyncEnumerable(async x =>
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(x));
+                    return x;
+                })
+                .ForEachAsync(x => Console.WriteLine("work {0}", x), CancellationToken.None)
+                ;
+
+            Console.WriteLine("doing something else");
+
+            await work;
+        }
+    }
+}
---
+++ src/System.Collections.Async/ToAsyncEnumerable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace System.Collections.Async
+{
+    public static partial class AsyncEnumerable
+    {
+        public static IAsyncEnumerable<TResult> ToAsyncEnumerable<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, Task<TResult>> selector)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            return new _AsyncEnumerable<TResult>(ct2 =>
+            {
+                var e = source.GetEnumerator();
+                return new _AsyncEnumerator<TResult>(async () =>
+                {
+                    if (e.MoveNext())
+                    {
+                        var x = selector(e.Current);
+                        return Tuple.Create(await x, true);
+                    }
+                    else
+                    {
+                        return Tuple.Create(default(TResult), false);
+                    }
+                });
+            });
+        }
+    }
+}
