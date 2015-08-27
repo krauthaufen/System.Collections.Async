3a8cb966392b74ab7812fa27a05b5e1c3ed9e171
Stefan Maierhofer <sm@stefanmaierhofer.com>
2015-08-27 21:30:30 +0200

    fixed buildNugetPackage.cmd

--- buildNugetPackage.cmd
+++ buildNugetPackage.cmd
@@ -1,4 +1,4 @@
-nuget\nuget restore ./src/System.Collections.Async.sln
+nuget\nuget restore ./src/Solution.sln
 msbuild ./src/Solution.sln /t:Build /p:Configuration="Release"
 mkdir build
 nuget\nuget pack ./src/System.Collections.Async.nuspec -OutputDirectory build
