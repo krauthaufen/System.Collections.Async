d95d92d1c0290b241a1c9cb9470d9e9ee2893cee
Stefan Maierhofer <sm@stefanmaierhofer.com>
2015-08-28 08:17:33 +0200

    added overloads to Sum operator

--- src/System.Collections.Async/Sum.cs
+++ src/System.Collections.Async/Sum.cs
@@ -9,12 +9,149 @@ namespace System.Collections.Async
 {
     public static partial class AsyncEnumerable
     {
+        public static async Task<decimal> SumAsync(this IAsyncEnumerable<decimal> source, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            var result = default(decimal);
+            var e = await source.GetEnumerator(ct);
+            while (await e.MoveNext(ct)) result += e.Current;
+            return result;
+        }
+        public static async Task<decimal?> SumAsync(this IAsyncEnumerable<decimal?> source, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            var result = default(decimal?);
+            var e = await source.GetEnumerator(ct);
+            while (await e.MoveNext(ct)) result += e.Current;
+            return result;
+        }
+        public static async Task<double> SumAsync(this IAsyncEnumerable<double> source, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            var result = default(double);
+            var e = await source.GetEnumerator(ct);
+            while (await e.MoveNext(ct)) result += e.Current;
+            return result;
+        }
+        public static async Task<double?> SumAsync(this IAsyncEnumerable<double?> source, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            var result = default(double?);
+            var e = await source.GetEnumerator(ct);
+            while (await e.MoveNext(ct)) result += e.Current;
+            return result;
+        }
+        public static async Task<float> SumAsync(this IAsyncEnumerable<float> source, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            var result = default(float);
+            var e = await source.GetEnumerator(ct);
+            while (await e.MoveNext(ct)) result += e.Current;
+            return result;
+        }
+        public static async Task<float?> SumAsync(this IAsyncEnumerable<float?> source, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            var result = default(float?);
+            var e = await source.GetEnumerator(ct);
+            while (await e.MoveNext(ct)) result += e.Current;
+            return result;
+        }
+        public static async Task<int> SumAsync(this IAsyncEnumerable<int> source, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            var result = default(int);
+            var e = await source.GetEnumerator(ct);
+            while (await e.MoveNext(ct)) result += e.Current;
+            return result;
+        }
+        public static async Task<int?> SumAsync(this IAsyncEnumerable<int?> source, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            var result = default(int?);
+            var e = await source.GetEnumerator(ct);
+            while (await e.MoveNext(ct)) result += e.Current;
+            return result;
+        }
+        public static async Task<long> SumAsync(this IAsyncEnumerable<long> source, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            var result = default(long);
+            var e = await source.GetEnumerator(ct);
+            while (await e.MoveNext(ct)) result += e.Current;
+            return result;
+        }
+        public static async Task<long?> SumAsync(this IAsyncEnumerable<long?> source, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            var result = default(long?);
+            var e = await source.GetEnumerator(ct);
+            while (await e.MoveNext(ct)) result += e.Current;
+            return result;
+        }
+        public static async Task<uint> SumAsync(this IAsyncEnumerable<uint> source, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            var result = default(uint);
+            var e = await source.GetEnumerator(ct);
+            while (await e.MoveNext(ct)) result += e.Current;
+            return result;
+        }
+        public static async Task<uint?> SumAsync(this IAsyncEnumerable<uint?> source, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            var result = default(uint?);
+            var e = await source.GetEnumerator(ct);
+            while (await e.MoveNext(ct)) result += e.Current;
+            return result;
+        }
+        public static async Task<ulong> SumAsync(this IAsyncEnumerable<ulong> source, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            var result = default(ulong);
+            var e = await source.GetEnumerator(ct);
+            while (await e.MoveNext(ct)) result += e.Current;
+            return result;
+        }
+        public static async Task<ulong?> SumAsync(this IAsyncEnumerable<ulong?> source, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            var result = default(ulong?);
+            var e = await source.GetEnumerator(ct);
+            while (await e.MoveNext(ct)) result += e.Current;
+            return result;
+        }
+
         public static async Task<decimal> SumAsync<TSource>(this IAsyncEnumerable<TSource> source, Func<TSource, decimal> selector, CancellationToken ct = default(CancellationToken))
         {
             if (source == null) throw new ArgumentNullException("source");
             if (selector == null) throw new ArgumentNullException("selector");
 
-            decimal result = 0;
+            var result = default(decimal);
+            var e = await source.GetEnumerator(ct);
+            while (await e.MoveNext(ct)) result += selector(e.Current);
+            return result;
+        }
+        public static async Task<decimal?> SumAsync<TSource>(this IAsyncEnumerable<TSource> source, Func<TSource, decimal?> selector, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            var result = default(decimal?);
             var e = await source.GetEnumerator(ct);
             while (await e.MoveNext(ct)) result += selector(e.Current);
             return result;
@@ -24,7 +161,17 @@ namespace System.Collections.Async
             if (source == null) throw new ArgumentNullException("source");
             if (selector == null) throw new ArgumentNullException("selector");
 
-            double result = 0;
+            var result = default(double);
+            var e = await source.GetEnumerator(ct);
+            while (await e.MoveNext(ct)) result += selector(e.Current);
+            return result;
+        }
+        public static async Task<double?> SumAsync<TSource>(this IAsyncEnumerable<TSource> source, Func<TSource, double?> selector, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            var result = default(double?);
             var e = await source.GetEnumerator(ct);
             while (await e.MoveNext(ct)) result += selector(e.Current);
             return result;
@@ -34,7 +181,17 @@ namespace System.Collections.Async
             if (source == null) throw new ArgumentNullException("source");
             if (selector == null) throw new ArgumentNullException("selector");
 
-            float result = 0;
+            var result = default(float);
+            var e = await source.GetEnumerator(ct);
+            while (await e.MoveNext(ct)) result += selector(e.Current);
+            return result;
+        }
+        public static async Task<float?> SumAsync<TSource>(this IAsyncEnumerable<TSource> source, Func<TSource, float?> selector, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            var result = default(float?);
             var e = await source.GetEnumerator(ct);
             while (await e.MoveNext(ct)) result += selector(e.Current);
             return result;
@@ -44,7 +201,17 @@ namespace System.Collections.Async
             if (source == null) throw new ArgumentNullException("source");
             if (selector == null) throw new ArgumentNullException("selector");
 
-            int result = 0;
+            var result = default(int);
+            var e = await source.GetEnumerator(ct);
+            while (await e.MoveNext(ct)) result += selector(e.Current);
+            return result;
+        }
+        public static async Task<int?> SumAsync<TSource>(this IAsyncEnumerable<TSource> source, Func<TSource, int?> selector, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            var result = default(int?);
             var e = await source.GetEnumerator(ct);
             while (await e.MoveNext(ct)) result += selector(e.Current);
             return result;
@@ -54,7 +221,17 @@ namespace System.Collections.Async
             if (source == null) throw new ArgumentNullException("source");
             if (selector == null) throw new ArgumentNullException("selector");
 
-            long result = 0;
+            var result = default(long);
+            var e = await source.GetEnumerator(ct);
+            while (await e.MoveNext(ct)) result += selector(e.Current);
+            return result;
+        }
+        public static async Task<long?> SumAsync<TSource>(this IAsyncEnumerable<TSource> source, Func<TSource, long?> selector, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            var result = default(long?);
             var e = await source.GetEnumerator(ct);
             while (await e.MoveNext(ct)) result += selector(e.Current);
             return result;
@@ -64,7 +241,17 @@ namespace System.Collections.Async
             if (source == null) throw new ArgumentNullException("source");
             if (selector == null) throw new ArgumentNullException("selector");
 
-            uint result = 0;
+            var result = default(uint);
+            var e = await source.GetEnumerator(ct);
+            while (await e.MoveNext(ct)) result += selector(e.Current);
+            return result;
+        }
+        public static async Task<uint?> SumAsync<TSource>(this IAsyncEnumerable<TSource> source, Func<TSource, uint?> selector, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            var result = default(uint?);
             var e = await source.GetEnumerator(ct);
             while (await e.MoveNext(ct)) result += selector(e.Current);
             return result;
@@ -74,7 +261,17 @@ namespace System.Collections.Async
             if (source == null) throw new ArgumentNullException("source");
             if (selector == null) throw new ArgumentNullException("selector");
 
-            ulong result = 0;
+            var result = default(ulong);
+            var e = await source.GetEnumerator(ct);
+            while (await e.MoveNext(ct)) result += selector(e.Current);
+            return result;
+        }
+        public static async Task<ulong?> SumAsync<TSource>(this IAsyncEnumerable<TSource> source, Func<TSource, ulong?> selector, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            var result = default(ulong?);
             var e = await source.GetEnumerator(ct);
             while (await e.MoveNext(ct)) result += selector(e.Current);
             return result;
