9330d340d3b5b45b63723de94c5ff43da759d7f9
Stefan Maierhofer <sm@stefanmaierhofer.com>
2015-08-28 07:26:19 +0200

    implemented Contains operator

---
+++ src/System.Collections.Async/Contains.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace System.Collections.Async
+{
+    public static partial class AsyncEnumerable
+    {
+        public static async Task<bool> ContainsAsync<TSource>(this IAsyncEnumerable<TSource> source, TSource value, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            var comparer = EqualityComparer<TSource>.Default;
+
+            var e = await source.GetEnumerator(ct);
+            while (await e.MoveNext(ct))
+            {
+                if (comparer.Equals(e.Current, value)) return true;
+            }
+            return false;
+        }
+
+        public static async Task<bool> ContainsAsync<TSource>(this IAsyncEnumerable<TSource> source, TSource value, IEqualityComparer<TSource> comparer, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            
+            var e = await source.GetEnumerator(ct);
+            while (await e.MoveNext(ct))
+            {
+                if (comparer.Equals(e.Current, value)) return true;
+            }
+            return false;
+        }
+    }
+}
