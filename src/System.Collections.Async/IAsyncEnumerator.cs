ee89845fd85e05e725b0cb88d27c3cbabf4e4af7
Stefan Maierhofer <sm@stefanmaierhofer.com>
2015-08-30 11:09:34 +0200

    refactored IAsyncEnumerator

--- src/System.Collections.Async/IAsyncEnumerator.cs
+++ src/System.Collections.Async/IAsyncEnumerator.cs
@@ -7,18 +7,87 @@ using System.Threading.Tasks;
 
 namespace System.Collections.Async
 {
-    public interface IAsyncEnumerator<out T>
+    public enum MoveNextStatus
+    {
+        Cancelled,
+        Completed,
+        Faulted,
+        Value,
+    }
+
+    public interface IMoveNextResult<T>
+    {
+        T Value { get; }
+
+        Exception Exception { get; }
+
+        MoveNextStatus Status { get; }
+
+        bool IsCancelled { get; }
+        bool IsCompleted { get; }
+        bool IsFaulted { get; }
+        bool IsValue { get; }
+    }
+
+    public class MoveNextCancelled<T> : IMoveNextResult<T>
+    {
+        public T Value { get { throw new InvalidOperationException(); } }
+
+        public Exception Exception => null;
+
+        public MoveNextStatus Status => MoveNextStatus.Cancelled;
+
+        public bool IsCancelled => true;
+        public bool IsCompleted => false;
+        public bool IsFaulted => false;
+        public bool IsValue => false;
+    }
+    public class MoveNextCompleted<T> : IMoveNextResult<T>
+    {
+        public T Value { get { throw new InvalidOperationException(); } }
+
+        public Exception Exception => null;
+
+        public MoveNextStatus Status => MoveNextStatus.Completed;
+
+        public bool IsCancelled => false;
+        public bool IsCompleted => true;
+        public bool IsFaulted => false;
+        public bool IsValue => false;
+    }
+    public class MoveNextException<T> : IMoveNextResult<T>
+    {
+        public T Value { get { throw new InvalidOperationException(); } }
+
+        public Exception Exception { get; }
+
+        public MoveNextStatus Status => MoveNextStatus.Faulted;
+
+        public bool IsCancelled => false;
+        public bool IsCompleted => false;
+        public bool IsFaulted => true;
+        public bool IsValue => false;
+    }
+    public class MoveNextValue<T> : IMoveNextResult<T>
+    {
+        public T Value { get; }
+
+        public Exception Exception => null;
+
+        public MoveNextStatus Status => MoveNextStatus.Value;
+
+        public bool IsCancelled => false;
+        public bool IsCompleted => false;
+        public bool IsFaulted => false;
+        public bool IsValue => true;
+    }
+    
+
+    public interface IAsyncEnumerator<T>
     {
         /// <summary>
-        /// Gets the current element in the collection.
-        /// </summary>
-        T Current { get; }
-
-        /// <summary>
         /// Asynchronously advances the enumerator to the next element of the collection.
-        /// Returns true if the enumerator successfully advanced to the next element;
-        /// false if it passed the end of the collection.
         /// </summary>
-        Task<bool> MoveNext(CancellationToken ct);
+        Task<IMoveNextResult<T>> MoveNext(CancellationToken ct);
     }
 }
