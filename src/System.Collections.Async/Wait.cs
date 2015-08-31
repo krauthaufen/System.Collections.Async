db9a6d7c47804651965b3acbb7126149e39e446e
Stefan Maierhofer <sm@stefanmaierhofer.com>
2015-08-31 09:24:23 +0200

    implemented Wait operator

---
+++ src/System.Collections.Async/Wait.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace System.Collections.Async
+{
+    public static partial class AsyncEnumerable
+    {
+        /// <summary>
+        /// Waits synchronously for the source to complete.
+        /// </summary>
+        public static void Wait<T>(this IAsyncEnumerable<T> source, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            var e = source.GetEnumerator(ct).Result;
+            ct.ThrowIfCancellationRequested();
+
+            var x = e.MoveNext(ct).Result;
+            while (x.IsValue)
+            {
+                ct.ThrowIfCancellationRequested();
+                x = e.MoveNext(ct).Result;
+            }
+            x.ThrowIfCancelledOrFaulted();
+            ct.ThrowIfCancellationRequested();
+        }
+    }
+}
