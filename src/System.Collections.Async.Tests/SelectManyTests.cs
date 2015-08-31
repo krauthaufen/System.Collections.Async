9c2ba350f088f0e81737430035d38c3d88c35b15
Stefan Maierhofer <sm@stefanmaierhofer.com>
2015-08-31 09:23:57 +0200

    refactored SelectMany

--- src/System.Collections.Async.Tests/SelectManyTests.cs
+++ src/System.Collections.Async.Tests/SelectManyTests.cs
@@ -1,78 +1,88 @@
-﻿//using System;
-//using System.Linq;
-//using System.Threading;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Linq;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-//namespace System.Collections.Async.Tests
-//{
-//    [TestClass]
-//    public class SelectManyTests
-//    {
-//        [TestMethod]
-//        public void ThrowsIfSourceIsNull()
-//        {
-//            try
-//            {
-//                AsyncEnumerable.SelectMany<int, int>(null, x => new[] { x }, CancellationToken.None);
-//                Assert.Fail();
-//            }
-//            catch (ArgumentNullException)
-//            {
-//            }
-//            catch
-//            {
-//                Assert.Fail();
-//            }
-//        }
-        
-//        [TestMethod]
-//        public void ThrowsIfSelectorIsNull()
-//        {
-//            try
-//            {
-//                var xs = AsyncEnumerable.Empty<int>();
-//                AsyncEnumerable.SelectMany<int, int>(xs, null, CancellationToken.None);
-//                Assert.Fail();
-//            }
-//            catch (ArgumentNullException)
-//            {
-//            }
-//            catch
-//            {
-//                Assert.Fail();
-//            }
-//        }
+namespace System.Collections.Async.Tests
+{
+    [TestClass]
+    public class SelectManyTests
+    {
+        [TestMethod]
+        public void SelectMany_ThrowsIfSourceIsNull()
+        {
+            try
+            {
+                AsyncEnumerable.SelectMany<int, int>(null, x => new[] { x }, CancellationToken.None);
+                Assert.Fail();
+            }
+            catch (ArgumentNullException)
+            {
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
+        [TestMethod]
+        public void SelectMany_ThrowsIfSelectorIsNull()
+        {
+            try
+            {
+                var xs = AsyncEnumerable.Empty<int>();
+                AsyncEnumerable.SelectMany<int, int>(xs, null, CancellationToken.None);
+                Assert.Fail();
+            }
+            catch (ArgumentNullException)
+            {
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
+        [TestMethod]
+        public void SelectMany_Cancelled()
+        {
+            TestHelpers.ThrowsAggregateTaskCanceledException(() =>
+                AsyncEnumerable.Empty<int[]>().SelectMany(x => x, TestHelpers.Cancelled).Wait()
+                );
+        }
+        [TestMethod]
+        public void SelectMany_Faulted()
+        {
+            TestHelpers.ThrowsAggregateTestException(() =>
+                TestHelpers.FaultedIntSequence.SelectMany(x => new[] { x }, CancellationToken.None).Wait()
+                );
+        }
 
-//        [TestMethod]
-//        public void EmptyCase()
-//        {
-//            var xs = AsyncEnumerable.Empty<int>();
-//            var rs = xs.SelectMany(x => new[] { x }, CancellationToken.None).ToArrayAsync(CancellationToken.None).Result;
-//            Assert.IsTrue(rs.Length == 0);
-//        }
-
-//        [TestMethod]
-//        public void SingleElement()
-//        {
-//            var xs = AsyncEnumerable.FromValue(42);
-//            var rs = xs.SelectMany(x => new[] { x, x * 2 }, CancellationToken.None).ToArrayAsync(CancellationToken.None).Result;
-//            Assert.IsTrue(rs.Length == 2 && rs[0] == 42 && rs[1] == 84);
-//        }
-
-//        [TestMethod]
-//        public void MultipleElements()
-//        {
-//            var xs = AsyncEnumerable.FromValues(new[] { 0, 1, 2, 3 });
-//            var rs = xs.SelectMany(x => Enumerable.Range(x, x), CancellationToken.None).ToArrayAsync(CancellationToken.None).Result;
-//            Assert.IsTrue(rs.Length == 6 && rs[0] == 1 && rs[1] == 2 && rs[2] == 3 && rs[3] == 3 && rs[4] == 4 && rs[5] == 5);
-//        }
-
-//        [TestMethod]
-//        public void TSourceIsDifferentFromTResult()
-//        {
-//            var xs = AsyncEnumerable.FromValues(new[] { 10, 20 });
-//            var rs = xs.SelectMany(x => new [] { "x", x.ToString() }, CancellationToken.None).ToArrayAsync(CancellationToken.None).Result;
-//            Assert.IsTrue(rs.Length == 4 && rs[0] == "x" && rs[1] == "10" && rs[2] == "x" && rs[3] == "20");
-//        }
-//    }
-//}
+        [TestMethod]
+        public void SelectMany_EmptyCase()
+        {
+            var xs = AsyncEnumerable.Empty<int>();
+            var rs = xs.SelectMany(x => new[] { x }).ToArray().Result;
+            Assert.IsTrue(rs.Length == 0);
+        }
+        [TestMethod]
+        public void SelectMany_SingleElement()
+        {
+            var xs = AsyncEnumerable.FromValue(42);
+            var rs = xs.SelectMany(x => new[] { x, x * 2 }).ToArray().Result;
+            Assert.IsTrue(rs.Length == 2 && rs[0] == 42 && rs[1] == 84);
+        }
+        [TestMethod]
+        public void SelectMany_MultipleElements()
+        {
+            var xs = AsyncEnumerable.FromValues(new[] { 0, 1, 2, 3 });
+            var rs = xs.SelectMany(x => Enumerable.Range(x, x)).ToArray().Result;
+            Assert.IsTrue(rs.Length == 6 && rs[0] == 1 && rs[1] == 2 && rs[2] == 3 && rs[3] == 3 && rs[4] == 4 && rs[5] == 5);
+        }
+        [TestMethod]
+        public void SelectMany_TSourceIsDifferentFromTResult()
+        {
+            var xs = AsyncEnumerable.FromValues(new[] { 10, 20 });
+            var rs = xs.SelectMany(x => new[] { "x", x.ToString() }).ToArray().Result;
+            Assert.IsTrue(rs.Length == 4 && rs[0] == "x" && rs[1] == "10" && rs[2] == "x" && rs[3] == "20");
+        }
+    }
+}
--- src/System.Collections.Async/SelectMany.cs
+++ src/System.Collections.Async/SelectMany.cs
@@ -1,46 +1,62 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
-//namespace System.Collections.Async
-//{
-//    public static partial class AsyncEnumerable
-//    {
-//        public static IAsyncEnumerable<TResult> SelectMany<TSource, TResult>(this IAsyncEnumerable<TSource> source, Func<TSource, IEnumerable<TResult>> selector, CancellationToken ct = default(CancellationToken))
-//        {
-//            if (source == null) throw new ArgumentNullException("source");
-//            if (selector == null) throw new ArgumentNullException("selector");
+namespace System.Collections.Async
+{
+    public static partial class AsyncEnumerable
+    {
+        public static IAsyncEnumerable<TResult> SelectMany<TSource, TResult>(this IAsyncEnumerable<TSource> source, Func<TSource, IEnumerable<TResult>> selector, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (selector == null) throw new ArgumentNullException("selector");
+            
+            return new _AsyncEnumerable<TResult>(async ct2 =>
+            {
+                var outer = await source.GetEnumerator(ct2);
+                ct.ThrowIfCancellationRequested();
+                ct2.ThrowIfCancellationRequested();
 
-//            return new _AsyncEnumerable<TResult>(async ct2 =>
-//            {
-//                var outer = await source.GetEnumerator(ct2);
-//                if (!await outer.MoveNext(ct2)) return new _AsyncEnumeratorEmpty<TResult>();
-//                var inner = selector(outer.Current).GetEnumerator();
-//                return new _AsyncEnumerator<TResult>(async () =>
-//                {
-//                    while (true)
-//                    {
-//                        if (inner.MoveNext())
-//                        {
-//                            return Tuple.Create(inner.Current, true);
-//                        }
-//                        else
-//                        {
-//                            if (await outer.MoveNext(ct2))
-//                            {
-//                                inner = selector(outer.Current).GetEnumerator();
-//                            }
-//                            else
-//                            {
-//                                return Tuple.Create(default(TResult), false);
-//                            }
-//                        }
-//                    }
-//                });
-//            });
-//        }
-//    }
-//}
+                var x = await outer.MoveNext(ct2);
+                ct.ThrowIfCancellationRequested();
+                ct2.ThrowIfCancellationRequested();
+                if (!x.IsValue)
+                {
+                    x.ThrowIfCancelledOrFaulted();
+                    return new _AsyncEnumeratorEmpty<TResult>();
+                }
+
+                var inner = selector(x.Value).GetEnumerator();
+                return new _AsyncEnumerator<TResult>(async () =>
+                {
+                    while (true)
+                    {
+                        if (inner.MoveNext())
+                        {
+                            return MoveNext.Value(inner.Current);
+                        }
+                        else
+                        {
+                            x = await outer.MoveNext(ct2);
+                            ct.ThrowIfCancellationRequested();
+                            ct2.ThrowIfCancellationRequested();
+
+                            if (x.IsValue)
+                            {
+                                inner = selector(x.Value).GetEnumerator();
+                            }
+                            else
+                            {
+                                x.ThrowIfCancelledOrFaulted();
+                                return MoveNext.Completed<TResult>();
+                            }
+                        }
+                    }
+                });
+            });
+        }
+    }
+}
