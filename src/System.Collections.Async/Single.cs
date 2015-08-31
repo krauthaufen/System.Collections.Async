d03b4646d588fa19926a457fa54c4f4f2a492587
Stefan Maierhofer <sm@stefanmaierhofer.com>
2015-08-31 21:47:12 +0200

    refactored Single, Take

--- src/System.Collections.Async/Single.cs
+++ src/System.Collections.Async/Single.cs
@@ -1,28 +1,33 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
-//namespace System.Collections.Async
-//{
-//    public static partial class AsyncEnumerable
-//    {
-//        public static async Task<TSource> SingleAsync<TSource>(this IAsyncEnumerable<TSource> source, CancellationToken ct = default(CancellationToken))
-//        {
-//            if (source == null) throw new ArgumentNullException("source");
-            
-//            var e = await source.GetEnumerator(ct);
-//            if (await e.MoveNext(ct))
-//            {
-//                var result = e.Current;
-//                if (!await e.MoveNext(ct))
-//                {
-//                    return result;
-//                }
-//            }
-//            throw new InvalidOperationException();
-//        }
-//    }
-//}
+namespace System.Collections.Async
+{
+    public static partial class AsyncEnumerable
+    {
+        public static async Task<TSource> Single<TSource>(this IAsyncEnumerable<TSource> source, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            ct.ThrowIfCancellationRequested();
+
+            var e = await source.GetEnumerator(ct);
+            if (e.Status != MoveNextStatus.None) throw new InvalidOperationException();
+
+            var x = await e.MoveNext(ct);
+            if (x.IsValue)
+            {
+                var result = x.Value;
+                x = await e.MoveNext(ct);
+                if (x.Status == MoveNextStatus.Completed)
+                {
+                    return result;
+                }
+            }
+            throw new InvalidOperationException();
+        }
+    }
+}
--- src/System.Collections.Async/Take.cs
+++ src/System.Collections.Async/Take.cs
@@ -1,34 +1,42 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
-//namespace System.Collections.Async
-//{
-//    public static partial class AsyncEnumerable
-//    {
-//        public static IAsyncEnumerable<TSource> Take<TSource>(this IAsyncEnumerable<TSource> source, int count, CancellationToken ct = default(CancellationToken))
-//        {
-//            if (source == null) throw new ArgumentNullException("source");
+namespace System.Collections.Async
+{
+    public static partial class AsyncEnumerable
+    {
+        public static IAsyncEnumerable<TSource> Take<TSource>(this IAsyncEnumerable<TSource> source, int count, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (ct.IsCancellationRequested) return _CanceledEnumerable<TSource>.Default;
 
-//            return new _AsyncEnumerable<TSource>(async ct2 =>
-//            {
-//                var taken = 0;
-//                var e = await source.GetEnumerator(ct2);
-//                return new _AsyncEnumerator<TSource>(async () =>
-//                {
-//                    if (taken++ < count && await e.MoveNext(ct))
-//                    {
-//                        return Tuple.Create(e.Current, true);
-//                    }
-//                    else
-//                    {
-//                        return Tuple.Create(default(TSource), false);
-//                    }
-//                });
-//            });
-//        }
-//    }
-//}
+            return new _AsyncEnumerable<TSource>(async ct2 =>
+            {
+                var taken = 0;
+                var e = await source.GetEnumerator(ct2);
+                switch (e.Status)
+                {
+                    case MoveNextStatus.Canceled:
+                        return _CanceledEnumerator<TSource>.Default;
+                    case MoveNextStatus.Faulted:
+                        return new _FaultedEnumerator<TSource>(e.Exception);
+                    case MoveNextStatus.Completed:
+                        return _EmptyEnumerator<TSource>.Default;
+                }
+
+                return new _AsyncEnumerator<TSource>(async () =>
+                {
+                    if (ct.IsCancellationRequested) return MoveNext<TSource>.Canceled;
+                    if (taken >= count) return MoveNext<TSource>.Completed;
+
+                    taken++;
+                    return await e.MoveNext(ct);
+                });
+            });
+        }
+    }
+}
