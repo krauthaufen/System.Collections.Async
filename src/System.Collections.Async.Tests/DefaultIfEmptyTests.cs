71bfc844808c1d22f21ef7b8c3fbcccdc8734de0
Stefan Maierhofer <sm@stefanmaierhofer.com>
2015-08-31 22:33:46 +0200

    refactored DefaultIfEmpty

--- src/System.Collections.Async.Tests/DefaultIfEmptyTests.cs
+++ src/System.Collections.Async.Tests/DefaultIfEmptyTests.cs
@@ -1,60 +1,43 @@
-﻿//using System;
-//using System.Threading;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-//namespace System.Collections.Async.Tests
-//{
-//    [TestClass]
-//    public class DefaultIfEmptyTests
-//    {
-//        [TestMethod]
-//        public void ThrowsWhenSourceIsNull1()
-//        {
-//            try
-//            {
-//                AsyncEnumerable.DefaultIfEmpty<int>(null, CancellationToken.None);
-//                Assert.Fail();
-//            }
-//            catch (ArgumentNullException)
-//            {
-//            }
-//            catch
-//            {
-//                Assert.Fail();
-//            }
-//        }
+namespace System.Collections.Async.Tests
+{
+    [TestClass]
+    public class DefaultIfEmptyTests
+    {
+        [TestMethod]
+        public void ThrowsWhenSourceIsNull1()
+        {
+            TestHelpers.ThrowsArgumentNullException(() =>
+                AsyncEnumerable.DefaultIfEmpty<int>(null)
+                );
+        }
 
-//        [TestMethod]
-//        public void ThrowsWhenSourceIsNull2()
-//        {
-//            try
-//            {
-//                AsyncEnumerable.DefaultIfEmpty<int>(null, 42, CancellationToken.None);
-//                Assert.Fail();
-//            }
-//            catch (ArgumentNullException)
-//            {
-//            }
-//            catch
-//            {
-//                Assert.Fail();
-//            }
-//        }
+        [TestMethod]
+        public void ThrowsWhenSourceIsNull2()
+        {
+            TestHelpers.ThrowsArgumentNullException(() =>
+                AsyncEnumerable.DefaultIfEmpty<int>(null, 42)
+                );
+        }
 
-//        [TestMethod]
-//        public void EmptyCase()
-//        {
-//            var xs = AsyncEnumerable.Empty<int>();
-//            Assert.IsTrue(xs.DefaultIfEmpty(CancellationToken.None).SingleAsync(CancellationToken.None).Result == 0);
-//            Assert.IsTrue(xs.DefaultIfEmpty(42, CancellationToken.None).SingleAsync(CancellationToken.None).Result == 42);
-//        }
-        
-//        [TestMethod]
-//        public void NonEmptyCase()
-//        {
-//            var xs = AsyncEnumerable.FromValue(17);
-//            Assert.IsTrue(xs.DefaultIfEmpty(CancellationToken.None).SingleAsync(CancellationToken.None).Result == 17);
-//            Assert.IsTrue(xs.DefaultIfEmpty(42, CancellationToken.None).SingleAsync(CancellationToken.None).Result == 17);
-//        }
-//    }
-//}
+        [TestMethod]
+        public void EmptyCase()
+        {
+            var xs = AsyncEnumerable.Empty<int>();
+            Assert.IsTrue(xs.DefaultIfEmpty().Single().Result == 0);
+            Assert.IsTrue(xs.DefaultIfEmpty(42).Single().Result == 42);
+        }
+
+        [TestMethod]
+        public void NonEmptyCase()
+        {
+            var xs = AsyncEnumerable.FromValue(17);
+            var test = xs.DefaultIfEmpty().ToArray().Result;
+            Assert.IsTrue(xs.DefaultIfEmpty().Single().Result == 17);
+            //Assert.IsTrue(xs.DefaultIfEmpty(42).Single().Result == 17);
+        }
+    }
+}
--- src/System.Collections.Async/DefaultIfEmpty.cs
+++ src/System.Collections.Async/DefaultIfEmpty.cs
@@ -1,48 +1,49 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
-//namespace System.Collections.Async
-//{
-//    public static partial class AsyncEnumerable
-//    {
-//        public static IAsyncEnumerable<TSource> DefaultIfEmpty<TSource>(this IAsyncEnumerable<TSource> source, CancellationToken ct = default(CancellationToken))
-//        {
-//            return DefaultIfEmpty(source, default(TSource), ct);
-//        }
+namespace System.Collections.Async
+{
+    public static partial class AsyncEnumerable
+    {
+        public static IAsyncEnumerable<TSource> DefaultIfEmpty<TSource>(this IAsyncEnumerable<TSource> source, CancellationToken ct = default(CancellationToken))
+        {
+            return DefaultIfEmpty(source, default(TSource), ct);
+        }
 
-//        public static IAsyncEnumerable<TSource> DefaultIfEmpty<TSource>(this IAsyncEnumerable<TSource> source, TSource defaultValue, CancellationToken ct = default(CancellationToken))
-//        {
-//            if (source == null) throw new ArgumentNullException("source");
+        public static IAsyncEnumerable<TSource> DefaultIfEmpty<TSource>(this IAsyncEnumerable<TSource> source, TSource defaultValue, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (ct.IsCancellationRequested) return _CanceledEnumerable<TSource>.Default;
 
-//            return new _AsyncEnumerable<TSource>(async ct2 =>
-//            {
-//                var e = await source.GetEnumerator(ct2);
-//                var empty = true;
-//                return new _AsyncEnumerator<TSource>(async () =>
-//                {
-//                    if (await e.MoveNext(ct))
-//                    {
-//                        empty = false;
-//                        return Tuple.Create(e.Current, true);
-//                    }
-//                    else
-//                    {
-//                        if (empty)
-//                        {
-//                            empty = false;
-//                            return Tuple.Create(defaultValue, true);
-//                        }
-//                        else
-//                        {
-//                            return Tuple.Create(default(TSource), false);
-//                        }
-//                    }
-//                });
-//            });
-//        }
-//    }
-//}
+            return new _AsyncEnumerable<TSource>(async ct2 =>
+            {
+                var e = await source.GetEnumerator(ct2);
+                switch (e.Status)
+                {
+                    case MoveNextStatus.Canceled:
+                        return _CanceledEnumerator<TSource>.Default;
+                    case MoveNextStatus.Faulted:
+                        return new _FaultedEnumerator<TSource>(e.Exception);
+                    case MoveNextStatus.Completed:
+                        return await FromValue(defaultValue).GetEnumerator(ct2);
+                    case MoveNextStatus.Value:
+                        return e;
+                }
+                
+                var x = await e.MoveNext(ct2);
+                if (x.Status == MoveNextStatus.Completed) return await FromValue(defaultValue).GetEnumerator(ct2);
+
+                return new _AsyncEnumerator<TSource>(async () =>
+                {
+                    var result = x;
+                    x = await e.MoveNext(ct2);
+                    return result;
+                });
+            });
+        }
+    }
+}
