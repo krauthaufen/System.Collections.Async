5963c16aa7b726e77615b8d2d6811c4e6157c10c
Stefan Maierhofer <sm@stefanmaierhofer.com>
2015-09-01 08:37:46 +0200

    refactored ElementAt, ElementAtOrDefault

--- src/System.Collections.Async/ElementAtOrDefault.cs
+++ src/System.Collections.Async/ElementAtOrDefault.cs
@@ -1,27 +1,49 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
-//namespace System.Collections.Async
-//{
-//    public static partial class AsyncEnumerable
-//    {
-//        public static async Task<TSource> ElementAtOrDefaultAsync<TSource>(this IAsyncEnumerable<TSource> source, int index, CancellationToken ct = default(CancellationToken))
-//        {
-//            if (source == null) throw new ArgumentNullException("source");
-//            if (index < 0) throw new ArgumentOutOfRangeException("index", index, "Negative index.");
+namespace System.Collections.Async
+{
+    public static partial class AsyncEnumerable
+    {
+        public static async Task<TSource> ElementAtOrDefaultAsync<TSource>(this IAsyncEnumerable<TSource> source, int index, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (index < 0) throw new ArgumentOutOfRangeException(nameof(index), index, "Negative index.");
+            ct.ThrowIfCancellationRequested();
 
-//            var i = 0;
+            var e = await source.GetEnumerator(ct);
+            switch (e.Status)
+            {
+                case MoveNextStatus.Canceled:
+                    throw new OperationCanceledException();
+                case MoveNextStatus.Faulted:
+                    throw e.Exception;
+            }
 
-//            var e = await source.GetEnumerator(ct);
-//            while (await e.MoveNext(ct))
-//            {
-//                if (i++ == index) return e.Current;
-//            }
-//            return default(TSource);
-//        }
-//    }
-//}
+            var i = 0;
+            var x = await e.MoveNext(ct);
+            while (x.IsValue)
+            {
+                ct.ThrowIfCancellationRequested();
+                if (i++ == index) return x.Value;
+                x = await e.MoveNext(ct);
+            }
+
+            switch (x.Status)
+            {
+                case MoveNextStatus.Canceled:
+                    throw new OperationCanceledException();
+                case MoveNextStatus.Faulted:
+                    throw x.Exception;
+                case MoveNextStatus.Completed:
+                    return default(TSource);
+            }
+
+            throw new InvalidOperationException();
+        }
+    }
+}
