ac0d1f40fc5258194b4c05dda061787b80dd35a4
Stefan Maierhofer <sm@stefanmaierhofer.com>
2015-09-02 08:47:06 +0200

    added AsEnumerable

---
+++ src/System.Collections.Async/AsEnumerable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace System.Collections.Async
+{
+    public static partial class AsyncEnumerable
+    {
+        /// <summary>
+        /// Synchronously enumerates all items.
+        /// </summary>
+        public static IEnumerable<TSource> AsEnumerable<TSource>(this IAsyncEnumerable<TSource> source, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
+            var e = source.GetEnumerator(ct).Result;
+            
+            var x = e.MoveNext(ct).Result;
+            while (x.IsValue)
+            {
+                x.ThrowIfCanceledOrFaulted();
+                ct.ThrowIfCancellationRequested();
+                yield return x.Value;
+                x = e.MoveNext(ct).Result;
+            }
+            x.ThrowIfCanceledOrFaulted();
+        }
+    }
+}
