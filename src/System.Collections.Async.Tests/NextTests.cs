7660b1178d79d2d0ebe378fd91275df1ab27d74a
Stefan Maierhofer <sm@stefanmaierhofer.com>
2015-09-03 09:35:09 +0200

    added Next operator

---
+++ src/System.Collections.Async.Tests/NextTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.Collections.Async.Tests
+{
+    [TestClass]
+    public class NextTests
+    {
+        [TestMethod]
+        public void ThrowsIfSourceIsNull()
+        {
+            TestHelpers.ThrowsAggregateArgumentNullException(() =>
+                AsyncEnumerable.Next<int>(null).Wait()
+                );
+        }
+
+        [TestMethod]
+        public void ThrowsIfSourceIsEmpty()
+        {
+            TestHelpers.ThrowsAggregateInvalidOperationException(() =>
+                AsyncEnumerable.Empty<int>().Next().Wait()
+                );
+        }
+
+        [TestMethod]
+        public void ThrowsIfSourceIsCanceled()
+        {
+            TestHelpers.ThrowsAggregateTaskCanceledException(() =>
+                AsyncEnumerable.Canceled<int>().Next().Wait()
+                );
+        }
+
+        [TestMethod]
+        public void ThrowsIfSourceIsFaulted()
+        {
+            TestHelpers.ThrowsAggregateTestException(() =>
+                AsyncEnumerable.Faulted<int>(new Exception("Test")).Next().Wait()
+                );
+        }
+
+        [TestMethod]
+        public void NextOfSource()
+        {
+            var source = AsyncEnumerable.Source<int>();
+            var next = source.Next();
+
+            var step = new ManualResetEventSlim(false);
+            var proceed = new ManualResetEventSlim(false);
+            
+            var t = Task.Run(async () =>
+            {
+                await source.Yield(1);
+                await Task.Yield();
+                step.Set();
+                proceed.Wait(); proceed.Reset();
+
+                await source.Yield(2);
+                await Task.Yield();
+                step.Set();
+                proceed.Wait(); proceed.Reset();
+
+                await source.Break();
+                await Task.Yield();
+                step.Set();
+                proceed.Wait(); proceed.Reset();
+            });
+
+            step.Wait(); step.Reset();
+            Assert.IsTrue(next.Result == 1, $"(1) {next.Result}");
+            next = source.Next();
+            proceed.Set();
+
+            step.Wait(); step.Reset();
+            Assert.IsTrue(next.Result == 2, $"(2) {next.Result}");
+            next = source.Next();
+            proceed.Set();
+
+            step.Wait(); step.Reset();
+        }
+    }
+}
---
+++ src/System.Collections.Async/Next.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace System.Collections.Async
+{
+    public static partial class AsyncEnumerable
+    {
+        public static async Task<TSource> Next<TSource>(this IAsyncEnumerable<TSource> source, CancellationToken ct = default(CancellationToken))
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            ct.ThrowIfCancellationRequested();
+            
+            var e = source.GetEnumerator(ct).Result;
+            switch (e.Status)
+            {
+                case MoveNextStatus.Canceled:
+                    throw new OperationCanceledException();
+
+                case MoveNextStatus.Completed:
+                    throw new InvalidOperationException();
+
+                case MoveNextStatus.Faulted:
+                    throw e.Exception;
+            }
+
+            var x = await e.MoveNext(ct);
+            if (x.IsValue) return x.Value;
+            x.ThrowIfCanceledOrFaulted();
+            throw new InvalidOperationException();
+        }
+    }
+}
